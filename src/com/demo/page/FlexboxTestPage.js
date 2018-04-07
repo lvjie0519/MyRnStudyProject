c156b6becf28d1570421f60b9d812da993e37a34
lvjie <991102@nd.com>
2018-04-07 16:31:37 +0800

    flexbox 使用方法

--- src/com/demo/page/FlexboxTestPage.js
+++ src/com/demo/page/FlexboxTestPage.js
@@ -7,22 +7,39 @@ import {
     StyleSheet,
     Text,
     View,
-    Image,
-    SrollView,
-    Button,
-    InputText
+    ScrollView,
 } from 'react-native';
 
 export default class FlexboxTestPage extends Component {
     render() {
         return (
-            <View>
+            <ScrollView>
 
-            </View>
+                <View style={{height:100, backgroundColor: '#b2d235', flexDirection:'row'}}>
+                    <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                    <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                    <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+                </View>
+
+                <View style={{height:100, backgroundColor: '#7fb80e', flexDirection:'row', justifyContent:'center'}}>
+                    <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                    <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                    <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+                </View>
+
+                <View style={{height:100, backgroundColor: '#6d8346', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                    <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                    <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                    <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+                </View>
+
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
+        viewStyle:{
 
+        }
 });
