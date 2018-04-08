e6db889a73d420d8a3277e072bccf0a06970c2b1
lvjie <991102@nd.com>
2018-04-08 20:39:40 +0800

    基础控件

--- src/com/demo/page/FlexboxTestPage.js
+++ src/com/demo/page/FlexboxTestPage.js
@@ -15,21 +15,33 @@ export default class FlexboxTestPage extends Component {
         return (
             <ScrollView>
 
-                <View style={{height:100, backgroundColor: '#b2d235', flexDirection:'row'}}>
+                <View>
+                    <View style={{flex:1, backgroundColor: 'powderblue'}} />
+                    <View style={{flex:2, backgroundColor: 'skyblue'}} />
+                    <View style={{flex:3, backgroundColor: 'steelblue'}} />
+                </View>
+
+                <View style={{height:100, marginTop:10}}>
+                    <View style={{flex:1, backgroundColor: 'powderblue'}} />
+                    <View style={{flex:2, backgroundColor: 'skyblue'}} />
+                    <View style={{flex:3, backgroundColor: 'steelblue'}} />
+                </View>
+
+                <View style={{height:100, backgroundColor: '#b2d235', flexDirection:'row', marginTop:10}}>
                     <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
                     <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
                     <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
                 </View>
 
                 <View style={{height:100, backgroundColor: '#7fb80e',
-                    flexDirection:'row', justifyContent:'center'}}>
+                    flexDirection:'row', justifyContent:'center', marginTop:10}}>
                     <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
                     <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
                     <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
                 </View>
 
                 <View style={{height:100, backgroundColor: '#6d8346',
-                    flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                    flexDirection:'row', justifyContent:'center', alignItems:'center', marginTop:10}}>
                     <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
                     <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
                     <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
